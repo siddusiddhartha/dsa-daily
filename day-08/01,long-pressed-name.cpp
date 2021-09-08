e2bc4f55fd5f20d819cb09f4bedd8c15f843c720
Siddhartha <sidduinevitable@gmail.com>
2021-09-08 21:17:04 +0530

    day-08: (problem-1) long pressed name

---
+++ day-08/01,long-pressed-name.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+  bool isLongPressedName(string name, string typed) {
+    int i = 0;
+    int m = name.size();
+    int n = typed.size();
+    for (int j = 0; j < n; j++) {
+      if (i < m && typed[j] == name[i]) {
+        i++;
+      } else if (!j || typed[j] != typed[j - 1]) {
+        return false;
+      }
+
+    }
+    return i == m;
+  }
+};
