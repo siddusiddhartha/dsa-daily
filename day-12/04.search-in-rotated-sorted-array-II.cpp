77e8bed950c6843bdf63f9933a64989eedb7c869
Siddhartha <sidduinevitable@gmail.com>
2021-09-18 18:39:29 +0530

    day-12: (problem 3) Search in rotated sorted array II

---
+++ day-12/04.search-in-rotated-sorted-array-II.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+  bool search(vector<int>& nums, int target) {
+    int low = 0;
+    int high = nums.size() - 1;
+    while (low <= high) {
+      int mid = low + (high - low) / 2;
+      if (nums[mid] == target) {
+        return true;
+      }
+      if (nums[mid] < nums[high]) {
+        if (nums[high] >= target && target >= nums[mid]) {
+          low = mid + 1;
+        } else {
+          high = mid - 1;
+        }
+      } else if (nums[mid] > nums[high]) {
+        if (nums[low] <= target && target <= nums[mid]) {
+          high = mid - 1;
+        } else {
+          low = mid + 1;
+        }
+      } else {
+        high--;
+      }
+    }
+    return false;
+  }
+};
